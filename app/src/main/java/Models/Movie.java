4efb501d42e4804634b89014d1736846e3f6e67b
jefffortune45 <jefffortune45@gmail.com>
2022-08-17 22:54:18 -0400

    my flixter2

--- app/src/main/java/Models/Movie.java
+++ app/src/main/java/Models/Movie.java
@@ -3,15 +3,20 @@ package Models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Parcel
 public class Movie {
+    int movieId;
     String backDropath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    public Movie(){}
 
 
     public Movie(JSONObject jsonObject) throws JSONException {
@@ -19,6 +24,8 @@ public class Movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
@@ -38,11 +45,21 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s",backDropath);
     }
 
+
+
     public String getTitle() {
         return title;
     }
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
